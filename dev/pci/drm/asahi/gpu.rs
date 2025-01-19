552345e5ec03cc5c759b478411b06f7c46eca88d
Yeri Kim <laplacianmatrix@gmail.com>
2025-01-19 15:05:57 +0900

    WIP

--- dev/pci/drm/asahi/gpu.rs
+++ dev/pci/drm/asahi/gpu.rs
@@ -302,7 +302,7 @@ impl rtkit::Operations for GpuManager::ver {
 
     fn recv_message(data: <Self::Data as ForeignOwnable>::Borrowed<'_>, ep: u8, msg: u64) {
         let dev = &data.dev;
-        //dev_info!(dev, "RtKit message: {:#x}:{:#x}\n", ep, msg);
+        dev_info!(dev, "RtKit message: {:#x}:{:#x}\n", ep, msg);
 
         if ep != EP_FIRMWARE || msg != MSG_RX_DOORBELL {
             dev_err!(dev, "Unknown message: {:#x}:{:#x}\n", ep, msg);
